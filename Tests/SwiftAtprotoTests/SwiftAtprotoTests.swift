d9d730d67b9b42b4ffc34d22dcae1efedb5aa75b
nnabeyang <nabeyang@gmail.com>
2023-10-11 06:18:13 +0900

    Initial commit

---
+++ Tests/SwiftAtprotoTests/SwiftAtprotoTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SwiftAtproto
+
+final class SwiftAtprotoTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
