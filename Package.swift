bce636c3f2a14cd7676d561181b1f9e52580c610
nnabeyang <nabeyang@gmail.com>
2024-04-26 13:20:46 +0900

    Bump up swift-argument-parser to 1.3.1

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", exact: "510.0.1"),
-        .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.2.3"),
+        .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.3.1"),
 
     ],
     targets: [
