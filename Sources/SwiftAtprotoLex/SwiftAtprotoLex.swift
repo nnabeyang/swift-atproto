00547782a4fc871d9086404b715a706616723902
nnabeyang <nabeyang@gmail.com>
2024-06-12 23:20:03 +0900

    v0.11.0

--- Sources/SwiftAtprotoLex/SwiftAtprotoLex.swift
+++ Sources/SwiftAtprotoLex/SwiftAtprotoLex.swift
@@ -2,7 +2,7 @@ import Foundation
 import SwiftSyntax
 import SwiftSyntaxBuilder
 
-public var version: String { "0.10.0" }
+public var version: String { "0.11.0" }
 
 public func main(outdir: String, path: String) throws {
     let decoder = JSONDecoder()
