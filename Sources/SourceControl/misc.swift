e9ea571a2b2aaeb792cf2f1d9ea18360450d90cb
nnabeyang <nabeyang@gmail.com>
2024-08-18 19:38:35 +0900

    v0.15.1

--- Sources/SourceControl/misc.swift
+++ Sources/SourceControl/misc.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public var version: String { "0.15.0" }
+public var version: String { "0.15.1" }
 
 public func getEnvSearchPaths(pathString: String) -> [URL] {
     pathString.split(separator: ":").map { URL(filePath: String($0)) }
