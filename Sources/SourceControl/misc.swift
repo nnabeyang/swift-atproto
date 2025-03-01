d769bb08dbab7c5e69bb86f929b6f971326038ec
noriaki watanabe <nabeyang@gmail.com>
2025-03-01 15:16:02 +0900

    v0.26.0

--- Sources/SourceControl/misc.swift
+++ Sources/SourceControl/misc.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public var version: String { "0.25.0" }
+public var version: String { "0.26.0" }
 
 public func getEnvSearchPaths(pathString: String) -> [URL] {
     pathString.split(separator: ":").map { URL(filePath: String($0)) }
