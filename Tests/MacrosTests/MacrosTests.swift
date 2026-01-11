cbd3dbac1535fb68a1d4956126059d150703b399
noriaki watanabe <nabeyang@gmail.com>
2026-01-11 13:48:14 +0900

    Limit Macro Tests to macOS to Fix Build Errors on Other Platforms

--- Tests/MacrosTests/MacrosTests.swift
+++ Tests/MacrosTests/MacrosTests.swift
@@ -5,53 +5,55 @@
 //  Created by Noriaki Watanabe on 2024/12/23.
 //
 
-import SwiftSyntax
-import SwiftSyntaxBuilder
-import SwiftSyntaxMacros
-import SwiftSyntaxMacrosTestSupport
-import XCTest
-@testable import Macros
+#if os(macOS)
+    import SwiftSyntax
+    import SwiftSyntaxBuilder
+    import SwiftSyntaxMacros
+    import SwiftSyntaxMacrosTestSupport
+    import XCTest
+    @testable import Macros
 
-final class MacroExampleTests: XCTestCase {
-    func testXRPCClientMacro() throws {
-        let testMacros: [String: Macro.Type] = [
-            "XRPCClientMacro": XRPCClientMacro.self,
-        ]
+    final class MacroExampleTests: XCTestCase {
+        func testXRPCClientMacro() throws {
+            let testMacros: [String: Macro.Type] = [
+                "XRPCClientMacro": XRPCClientMacro.self,
+            ]
 
-        assertMacroExpansion(
-            """
-            @XRPCClientMacro
-            public struct TestClient {
-              let host: URL
-              public var auth: AuthInfo
-              public var serviceEndpoint: URL {
-                auth.serviceEndPoint ?? host
-              }
-              public let decoder: JSONDecoder
-            }
-            """,
-            expandedSource: """
-            public struct TestClient {
-              let host: URL
-              public var auth: AuthInfo
-              public var serviceEndpoint: URL {
-                auth.serviceEndPoint ?? host
-              }
-              public let decoder: JSONDecoder
+            assertMacroExpansion(
+                """
+                @XRPCClientMacro
+                public struct TestClient {
+                  let host: URL
+                  public var auth: AuthInfo
+                  public var serviceEndpoint: URL {
+                    auth.serviceEndPoint ?? host
+                  }
+                  public let decoder: JSONDecoder
+                }
+                """,
+                expandedSource: """
+                public struct TestClient {
+                  let host: URL
+                  public var auth: AuthInfo
+                  public var serviceEndpoint: URL {
+                    auth.serviceEndPoint ?? host
+                  }
+                  public let decoder: JSONDecoder
 
-              private init(host: URL, auth: AuthInfo) {
-                self.host = host
-                self.auth = auth
-                decoder = JSONDecoder()
-                Self.setModuleName()
-              }
-            }
+                  private init(host: URL, auth: AuthInfo) {
+                    self.host = host
+                    self.auth = auth
+                    decoder = JSONDecoder()
+                    Self.setModuleName()
+                  }
+                }
 
-            extension TestClient: XRPCClientProtocol {
-            }
-            """,
-            macros: testMacros,
-            indentationWidth: .spaces(2)
-        )
+                extension TestClient: XRPCClientProtocol {
+                }
+                """,
+                macros: testMacros,
+                indentationWidth: .spaces(2)
+            )
+        }
     }
-}
+#endif
