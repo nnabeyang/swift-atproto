b7ba5b162e8722a5ef7d835cb39f06e593b29e4e
noriaki watanabe <nabeyang@gmail.com>
2026-02-01 08:44:43 +0900

    Separate file enumeration from schema decoding

--- Sources/SwiftAtprotoLex/SwiftAtprotoLex.swift
+++ Sources/SwiftAtprotoLex/SwiftAtprotoLex.swift
@@ -3,23 +3,27 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 
 public func main(outdir: String, path: String) throws {
-  let decoder = JSONDecoder()
-  var schemas = [Schema]()
   let url = URL(filePath: path)
-  var prefixes = Set<String>()
+  var fileURLs = [URL]()
   if let enumerator = FileManager.default.enumerator(at: url, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
     for case let fileUrl as URL in enumerator {
       do {
         let fileAttributes = try fileUrl.resourceValues(forKeys: [.isRegularFileKey])
         if fileAttributes.isRegularFile!, fileUrl.pathExtension == "json" {
-          prefixes.insert(fileUrl.prefix(baseURL: url))
-          let data = try Data(contentsOf: fileUrl)
-          try schemas.append(decoder.decode(Schema.self, from: data))
+          fileURLs.append(fileUrl)
         }
       } catch {
         print(error, fileUrl)
       }
     }
+  }
+  let decoder = JSONDecoder()
+  var schemas = [Schema]()
+  var prefixes = Set<String>()
+  for fileUrl in fileURLs {
+    let data = try Data(contentsOf: fileUrl)
+    try schemas.append(decoder.decode(Schema.self, from: data))
+    prefixes.insert(fileUrl.prefix(baseURL: url))
   }
   let defmap = Lex.buildExtDefMap(schemas: schemas, prefixes: prefixes)
   let outdirBaseURL = URL(filePath: outdir)
