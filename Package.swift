11c0cfed96ace35b20691d9d248ea823a374ba0b
noriaki watanabe <nabeyang@gmail.com>
2026-02-01 16:25:24 +0900

    Bump macOS min deployment to v14
    Increase the macOS minimum from v13 to v14. Needed to support
    newer decoding/Encode/Decode usages, including custom
    DecodableWithConfiguration logic that depend on the macOS 14 SDK.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftAtproto",
-  platforms: [.macOS(.v13), .iOS(.v16)],
+  platforms: [.macOS(.v14), .iOS(.v16)],
   products: [
     .library(
       name: "SwiftAtproto",
