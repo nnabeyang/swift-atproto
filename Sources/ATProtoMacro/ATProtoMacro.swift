c19e2bfa6af2246632d2f65eeac06788ac162d32
noriaki watanabe <nabeyang@gmail.com>
2026-04-30 21:47:31 +0900

    Deprecate XRPCClient macro and transition to _XRPCClientProtocol

--- Sources/ATProtoMacro/ATProtoMacro.swift
+++ Sources/ATProtoMacro/ATProtoMacro.swift
@@ -7,6 +7,7 @@
 
 import SwiftAtproto
 
+@available(*, deprecated, message: "Use the code-generated `XRPCClientProtocol` instead. `_XRPCClientProtocol` only provides internal infrastructure and lacks XRPC API method requirements.")
 @attached(member, names: named(init))
 @attached(extension, conformances: _XRPCClientProtocol)
 public macro XRPCClient() = #externalMacro(module: "Macros", type: "XRPCClientMacro")
