18c7aed7af7691278f335f3b5c358dcbebec503c
noriaki watanabe <nabeyang@gmail.com>
2026-05-02 08:37:52 +0900

    v0.38.0

--- Sources/SourceControl/misc.swift
+++ Sources/SourceControl/misc.swift
@@ -1,7 +1,7 @@
 import Crypto
 import Foundation
 
-public var version: String { "0.37.1" }
+public var version: String { "0.38.0" }
 
 public func getEnvSearchPaths(pathString: String) -> [URL] {
   pathString.split(separator: ":").map { URL(filePath: String($0)) }
