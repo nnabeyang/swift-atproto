1ce8c4d888fa6765ee6347349c0c015951c386c6
noriaki watanabe <nabeyang@gmail.com>
2026-06-28 11:00:00 +0900

    feat: add init(string:strict:) and typedLenient accessor

--- Sources/SwiftAtproto/StringFormats/LexiconStringFormat.swift
+++ Sources/SwiftAtproto/StringFormats/LexiconStringFormat.swift
@@ -3,6 +3,14 @@ import Foundation
 public protocol LexiconStringFormat: Hashable, Sendable {
   var rawValue: String { get }
   init(string: String) throws
+  init(string: String, strict: Bool) throws
+}
+
+extension LexiconStringFormat {
+  // Formats without a lenient override (most identifier formats) fall through to strict.
+  public init(string: String, strict: Bool) throws {
+    try self.init(string: string)
+  }
 }
 
 public enum LexiconStringFormatError: Error, Equatable {
