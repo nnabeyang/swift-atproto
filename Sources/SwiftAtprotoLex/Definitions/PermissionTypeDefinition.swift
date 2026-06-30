cb3ca9b9a01ca57b939dcee2a4e80f051a1afeae
noriaki watanabe <nabeyang@gmail.com>
2026-06-30 20:38:03 +0900

    feat: introduce PermissionAction newtype for action field

--- Sources/SwiftAtprotoLex/Definitions/PermissionTypeDefinition.swift
+++ Sources/SwiftAtprotoLex/Definitions/PermissionTypeDefinition.swift
@@ -4,7 +4,7 @@ struct PermissionTypeDefinition: Codable {
   let aud: String?
   let inheritAud: Bool?
   let lxm: [String]?
-  let action: [String]?
+  let action: [PermissionAction]?
   let collection: [String]?
 }
 
@@ -26,4 +26,25 @@ struct PermissionResource: RawRepresentable, Codable, Hashable, Sendable {
 
   static let rpc = Self(rawValue: "rpc")
   static let repo = Self(rawValue: "repo")
+}
+
+struct PermissionAction: RawRepresentable, Codable, Hashable, Sendable {
+  let rawValue: String
+
+  init(rawValue: String) {
+    self.rawValue = rawValue
+  }
+
+  init(from decoder: Decoder) throws {
+    self.rawValue = try decoder.singleValueContainer().decode(String.self)
+  }
+
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(rawValue)
+  }
+
+  static let create = Self(rawValue: "create")
+  static let update = Self(rawValue: "update")
+  static let delete = Self(rawValue: "delete")
 }
