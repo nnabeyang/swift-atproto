2c8e57df5ba8cbc62c7bf6d7be33282381f2517f
noriaki watanabe <nabeyang@gmail.com>
2026-06-30 20:37:30 +0900

    feat: introduce PermissionResource newtype for resource field

--- Sources/SwiftAtprotoLex/Definitions/PermissionTypeDefinition.swift
+++ Sources/SwiftAtprotoLex/Definitions/PermissionTypeDefinition.swift
@@ -1,9 +1,29 @@
 struct PermissionTypeDefinition: Codable {
   var type: FieldType { .permission }
-  let resource: String
+  let resource: PermissionResource
   let aud: String?
   let inheritAud: Bool?
   let lxm: [String]?
   let action: [String]?
   let collection: [String]?
+}
+
+struct PermissionResource: RawRepresentable, Codable, Hashable, Sendable {
+  let rawValue: String
+
+  init(rawValue: String) {
+    self.rawValue = rawValue
+  }
+
+  init(from decoder: Decoder) throws {
+    self.rawValue = try decoder.singleValueContainer().decode(String.self)
+  }
+
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(rawValue)
+  }
+
+  static let rpc = Self(rawValue: "rpc")
+  static let repo = Self(rawValue: "repo")
 }
